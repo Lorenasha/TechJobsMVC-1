9c7aba1eb474315f91bb6dff71b756257b50cc6f
Lorenasha <lorenasha_01@hotmail.com>
2019-12-12 20:20:41 -0500

    Copy working project

--- src/TechJobs/Controllers/ListController.cs
+++ src/TechJobs/Controllers/ListController.cs
@@ -13,7 +13,6 @@ namespace TechJobs.Controllers
         // to initialize static members of a class
         static ListController() 
         {
-            
             columnChoices.Add("core competency", "Skill");
             columnChoices.Add("employer", "Employer");
             columnChoices.Add("location", "Location");
@@ -26,6 +25,7 @@ namespace TechJobs.Controllers
             ViewBag.columns = columnChoices;
             return View();
         }
+
 
         public IActionResult Values(string column)
         {
--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -6,6 +6,7 @@ namespace TechJobs.Controllers
 {
     public class SearchController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             ViewBag.columns = ListController.columnChoices;
@@ -15,6 +16,34 @@ namespace TechJobs.Controllers
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
+        [HttpPost]
+        [Route("/Search")]
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            ViewBag.columns = ListController.columnChoices;
+            ViewBag.title = "Search";
+
+            if (searchType.Equals("all"))
+            {
+                List<Dictionary<string, string>> jobs = JobData.FindByValue(searchTerm);
+                ViewBag.title = "All Jobs by " + searchTerm + " Value";
+                ViewBag.searchTerm = searchTerm;
+                ViewBag.jobs = jobs;
+              
+            }
+            else
+            {
+                List<Dictionary<string, string>> jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.title = "All " + searchType + " With " + searchTerm +" Values";
+                ViewBag.searchType = searchType;
+                ViewBag.searchTerm = searchTerm;
+                ViewBag.jobs = jobs;
+               
+            }
+
+            return View("Index");
+        }
+
 
     }
 }
